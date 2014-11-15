ce8880c4ce9ab1d182483da8be47d4ec5a2f28e9
Mads André Lundeland <lundeland@gmail.com>
2014-11-15 14:10:13 +0100

    Implement GameMaster and Game actors

--- scalajvm/app/models/GameMaster.scala
+++ scalajvm/app/models/GameMaster.scala
@@ -1,12 +1,38 @@
 package models
 
-import akka.actor.Actor
-import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef}
+import shared.{GameStart, CreateGame}
 
-/*class GameMaster extends Actor {
+import scala.util.Random
 
-  override def receive = {
-    case _ => //TODO
+
+class GameMaster extends Actor {
+
+  import shared.JoinGame
+
+  var pendingGames: Set[ActorRef] = Set()
+
+  override def receive: Receive = {
+
+    case msg: JoinGame =>
+      val game: ActorRef = findOrCreateGame()
+      game forward msg
+
+    case JoinGame(playerName, Some(gameId)) =>
+    case CreateGame(player) =>
+
+    case GameStart(_, _, _) =>
+      pendingGames = pendingGames.filterNot(_ == sender)
   }
 
-}*/
+
+  def findOrCreateGame(): ActorRef = {
+    if (pendingGames.isEmpty) {
+      val gameId: Long = Random.nextLong()
+      val game: ActorRef = context.actorOf(Game.props(gameId), name = s"game-$gameId")
+      pendingGames += game
+    }
+    pendingGames.head
+  }
+
+}
