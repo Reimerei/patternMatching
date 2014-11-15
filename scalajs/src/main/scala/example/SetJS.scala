fd2c1820f6d4d729a5d12a05d58cb8f9413d4698
reimerei <github@bjrm.de>
2014-11-15 15:10:00 +0100

    client websocket connection work kind of

--- scalajs/src/main/scala/example/SetJS.scala
+++ scalajs/src/main/scala/example/SetJS.scala
@@ -20,7 +20,6 @@ object SetJS {
   @JSExport
   def main(wsUrl: String) = {
     val connection = new WSConnection(wsUrl)
-    connection.send(CreateGame("foo"))
   }
 
   class WSConnection(url: String) {
@@ -28,7 +27,8 @@ object SetJS {
     PicklerRegistry.register[CreateGame]
 
     val socket = new dom.WebSocket(url)
-//    socket.onmessage = receive _
+    socket.onmessage = receive _
+    socket.onopen = onStart _
 
     def send(message: ClientSends) = {
       val json = PicklerRegistry.pickle(message)
@@ -39,6 +39,10 @@ object SetJS {
     def receive(e: dom.MessageEvent) = {
       val json = js.JSON.parse(e.data.toString)
       println(json)
+    }
+
+    def onStart(e: dom.Event) = {
+      this.send(CreateGame("foo"))
     }
 
 
