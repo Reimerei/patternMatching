ac2192765e3d4717605f081dd97f622f5f25bbef
reimerei <github@bjrm.de>
2014-11-15 18:39:33 +0100

    more work

--- scalajs/src/main/scala/example/SetJS.scala
+++ scalajs/src/main/scala/example/SetJS.scala
@@ -1,13 +1,13 @@
 package example
 
 import org.scalajs.dom
-import org.scalajs.jquery.{jQuery => $}
+import org.scalajs.jquery.{ jQuery => $ }
 import org.scalajs.spickling.PicklerRegistry
 import org.scalajs.spickling.jsany._
 import shared._
 
 import scala.scalajs.js
-import scala.scalajs.js.Dynamic.{global => g}
+import scala.scalajs.js.Dynamic.{ global => g }
 import scala.scalajs.js.JSON
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom._
@@ -37,7 +37,6 @@ object SetJS {
     }
 
     def receive(e: dom.MessageEvent) = {
-      println("received: " + e.data.toString)
       val json = js.JSON.parse(e.data.toString).asInstanceOf[js.Any]
       PicklerRegistry.unpickle(json) match {
         case GameStart(cards, scoreCard, gameId) =>
@@ -54,7 +53,7 @@ object SetJS {
 
     def onStart(e: dom.Event) = {
       val content = dom.document.getElementById("content")
-      content.appendChild(WebElements.enterName(this).render)
+      content.appendChild(WebElements.enterName().render)
     }
 
     def joinGame(name: String) = {
@@ -62,62 +61,70 @@ object SetJS {
       val content = dom.document.getElementById("content")
       content.innerHTML = ""
       content.appendChild(WebElements.waitingForGame.render)
-      val cards = Set(Card(List(1,2,3,4)))
+      val cards = Set(Card(List(1, 2, 3, 4)))
       content.appendChild(WebElements.displayGame(cards).render)
     }
 
-    def updateBoard(newCards : Set[Card]) = {
+    def updateBoard(newCards: Set[Card]) = {
       //TODO
     }
 
-    def updateScoreCard(scoreCard : Map[Player, Int]) = {
+    def updateScoreCard(scoreCard: Map[Player, Int]) = {
       //TODO
     }
 
+    object WebElements {
+
+      def enterName() = div(id := "signInPanel") {
+        form(`class` := "form-inline", "role".attr := "form")(
+          div(id := "usernameForm", `class` := "form-group")(
+            div(`class` := "input-group")(
+              div(`class` := "input-group-addon", raw("&#9786;")),
+              input(id := "username", `class` := "form-control", `type` := "text", placeholder := "Enter username")
+            )
+          ),
+          span(style := "margin:0px 5px"),
+          button(`class` := "btn btn-default", onclick := { () =>
+            val input = $("#username").value().toString.trim
+            if (input == "") {
+              $("#usernameForm").addClass("has-error")
+              dom.alert("Invalid username")
+            } else {
+              joinGame(input)
+            }
+            false
+          })("Sign in")
+        )
+      }
+
+      def waitingForGame = div() {
+        "Waiting For Game..."
+      }
+
+      def singleCard(card: Card) = div(`class` := "card", onclick := { () =>
+
+
+        if ($("this").hasClass("select")) {
+          $("this").removeClass("select")
+        } else {
+          $("this").addClass("select")
+        }
+
+        if ($(".select").length == 3) {
+         println($(".select").attr("value"))
+          // todo send to backend
+
+          $(".select").removeClass("select")
+        }
+
+      }) {
+        card.id.mkString(", ")
+      }
+
+      def displayGame(cards: Set[Card]) = div(`class` := "board") {
+        cards.toSeq.map(singleCard)
+      }
+    }
   }
-
-  object WebElements {
-
-    def enterName(client: SetClient) = div(id := "signInPanel") {
-      form(`class` := "form-inline", "role".attr := "form")(
-        div(id := "usernameForm", `class` := "form-group")(
-          div(`class` := "input-group")(
-            div(`class` := "input-group-addon", raw("&#9786;")),
-            input(id := "username", `class` := "form-control", `type` := "text", placeholder := "Enter username")
-          )
-        ),
-        span(style := "margin:0px 5px"),
-        button(`class` := "btn btn-default", onclick := { () =>
-          val input = $("#username").value().toString.trim
-          if (input == "") {
-            $("#usernameForm").addClass("has-error")
-            dom.alert("Invalid username")
-          } else {
-             client.joinGame(input)
-          }
-          false
-        })("Sign in")
-      )
-    }
-
-    def waitingForGame = div(){
-      "Waiting For Game..."
-    }
-
-    def singleCard(card: Card) = div(`class` := "card", onclick := { () =>
-      println("clicked")
-     $("this").addClass("selected")
-    } ){
-       card.id.mkString(", ")
-    }
-
-    def displayGame(cards: Set[Card])  = div(`class` := "board"){
-      cards.toSeq.map(singleCard)
-    }
-
-  }
-
-
-
 }
 
