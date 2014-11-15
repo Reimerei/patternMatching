23dbe46e344529ad4e1d59c26d2da2f586c57680
Mads André Lundeland <lundeland@gmail.com>
2014-11-15 12:00:23 +0100

    Shared case classes

--- scala/src/main/scala/shared/SharedMessages.scala
+++ scala/src/main/scala/shared/SharedMessages.scala
@@ -3,3 +3,20 @@ package shared
 object SharedMessages {
   def itWorks = "It works!"
 }
+
+
+
+
+case class Player(name: String)
+case class Card(id: Seq[Int])
+
+// Client receives
+case class GameStart(cards: Set[Card], scoreCard: Map[Player, Int], gameId: Long)
+case class SetCompleted(cards: Set[Card], scoreCard: Map[Player, Int])
+case object WrongGuess
+case class GameFinished(scoreCard: Map[Player, Int])
+
+// Client sends
+case class Guess(cards: Set[Card])
+case class JoinGame(playerName: String, gameId: Option[Long])
+case class CreateGame(playerName: String)
---
+++ scalajvm/app/models/GameMaster.scala
@@ -0,0 +1,12 @@
+package models
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+class GameMaster extends Actor {
+
+  override def receive: Receive = {
+
+  }
+
+}
