86973fa4fc5c56af2b742019278cbd9081356c44
Everett Sochowski <everett.sochowski@gmail.com>
2014-11-15 14:02:37 +0100

    Add logging for when socket receives valid messages

--- scalajvm/app/models/UserActor.scala
+++ scalajvm/app/models/UserActor.scala
@@ -1,10 +1,21 @@
 package models
 
 import akka.actor.{Props, ActorRef, Actor}
-import play.api.libs.json.JsValue
+import play.api.Logger
+import play.api.libs.json.{JsValue}
+import org.scalajs.spickling._
 import shared._
+import org.scalajs.spickling.playjson._
 
 object UserActor {
+  PicklerRegistry.register[GameStart]
+  PicklerRegistry.register[SetCompleted]
+  PicklerRegistry.register(WrongGuess)
+  PicklerRegistry.register[GameFinished]
+  PicklerRegistry.register[Guess]
+  PicklerRegistry.register[JoinGame]
+  PicklerRegistry.register[CreateGame]
+
   def props(out : ActorRef) = Props(new UserActor(out))
 }
 
@@ -14,13 +25,14 @@ class UserActor(out : ActorRef) extends Actor {
   def receive = {
     //from websocket
     case msg : JsValue => {
-      val unpickled = ??? //TODO
+      //val unpickled = PicklerRegistry.unpickle(msg) //TODO
       //pass it on
+      Logger.debug(s"Received message: $msg")
     }
     //from game logic
-    case msg : Any => {
+    case msg : Any => { //TODO: Any -> ServerSend
       //TODO: handle pickling error?
-      val pickled : JsValue = ???
+      val pickled : JsValue = PicklerRegistry.pickle(msg)
       out ! pickled
     }
   }
