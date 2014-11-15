8c19de91c854a421a4fa51c628d6df892cc67685
Everett Sochowski <everett.sochowski@gmail.com>
2014-11-15 13:40:19 +0100

    Work on pickling/unpickling

--- scalajvm/app/models/UserActor.scala
+++ scalajvm/app/models/UserActor.scala
@@ -2,17 +2,26 @@ package models
 
 import akka.actor.{Props, ActorRef, Actor}
 import play.api.libs.json.JsValue
+import shared._
 
 object UserActor {
   def props(out : ActorRef) = Props(new UserActor(out))
 }
 
+//Handles pickling and unpickling
 class UserActor(out : ActorRef) extends Actor {
 
   def receive = {
     //from websocket
     case msg : JsValue => {
-      //TODO: unpickle
+      val unpickled = ??? //TODO
+      //pass it on
+    }
+    //from game logic
+    case msg : Any => {
+      //TODO: handle pickling error?
+      val pickled : JsValue = ???
+      out ! pickled
     }
   }
 
